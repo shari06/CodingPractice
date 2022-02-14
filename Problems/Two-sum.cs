8ea4efb11f27ecbcae51c98759bf4e438e11b258
Srihari Rajendran <sriharirajendran@live.com>
2022-02-14 00:37:20 -0500

    Two-sum solved

---
+++ Problems/Two-sum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice.Problems
+{
+    internal class Two_sum
+    { 
+        public int[] TwoSum(int[] nums, int target)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result.Add(i);
+                        result.Add(j);
+                        break;
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
